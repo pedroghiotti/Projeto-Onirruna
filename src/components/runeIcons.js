3c725122e10c2b5945c299b8d9b633841479c39b
pedroogaab <plechi_2016@hotmail.com>
2023-11-30 11:40:04 -0300

    adicionei o arquivo runeIcons onde estao todas as runas do lol, e coloquei uma runa padrao em todos os campeoes no json

---
+++ src/components/runeIcons.js
@@ -0,0 +1,99 @@
+const runeIcons = {
+    "Precisao": {
+        "pressioneAtk": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/presstheattack/presstheattack.png'),
+        "ritmoFatal": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/lethaltempo/lethaltempotemp.png'),
+        "agilidade": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/fleetfootwork/fleetfootwork.png'),
+        "conquistador": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/conqueror/conqueror.png'),
+        
+        "curaEx": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/overheal.png'),
+        "triunfo": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/triumph.png'),
+        "presencaEsp": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/presenceofmind/presenceofmind.png'),
+
+        "espontaneidade": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/legendalacrity/legendalacrity.png'),
+        "linhagem": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/legendbloodline/legendbloodline.png'),
+        "tenacidade": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/legendtenacity/legendtenacity.png'),
+        
+        "golpeMisericordia": requite('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/coupdegrace/coupdegrace.png'),
+        "dilacerar": requite('https://raw.communitydragon.org/latest/game/assets/perks/styles/precision/cutdown/cutdown.png'),
+        "ateMorte": require('https://ruined.dev/_next/image?url=https%3A%2F%2Fruined.dev%2Fstatic%2Fperk-images%2FStyles%2FSorcery%2FLastStand%2FLastStand.png&w=128&q=75'),
+    },
+    
+    "Dominacao": {
+
+        "eletrocutar": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/electrocute/electrocute.png'),
+        "predador": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/predator/predator.png'),
+        "colheitaSombria": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/darkharvest/darkharvest.png'),
+        "chuvaLaminas": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/hailofblades/hailofblades.png'),
+
+        "golspeDesleal": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/cheapshot/cheapshot.png'),
+        "gostoSangue": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/tasteofblood/greenterror_tasteofblood.png'),
+        "impactoRepentino": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/suddenimpact/suddenimpact.png'),
+
+        "sentinelaZumbi": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/zombieward/zombieward.png'),
+        "poroFantasma": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/ghostporo/ghostporo.png'),
+        "globosOculares": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/eyeballcollection/eyeballcollection.png'),
+
+        "cacadorTesouros": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/treasurehunter/treasurehunter.png'),
+        "cacaArdilosa": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/ingenioushunter/ingenioushunter.png'),
+        "cacaIncansavel": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/relentlesshunter/relentlesshunter.png'),
+        "cacaSuprema": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/domination/ultimatehunter/ultimatehunter.png'),
+
+    },
+    "Feiticaria": {
+
+        "invocarAery": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/summonaery/summonaery.png'),
+        "cometaArcano": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/arcanecomet/arcanecomet.png'),
+        "impetoGradual": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/phaserush/phaserush.png'),
+
+        "orbeAnulador": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/nullifyingorb/pokeshield.png'),
+        "faixaFluxoMana": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/manaflowband/manaflowband.png'),
+        "mantoNimbus": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/nimbuscloak/6361.png'),
+
+        "transcendencia": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/transcendence/transcendence.png'),
+        "celeridade": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/celerity/celeritytemp.png'),
+        "focoAbsoluto": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/absolutefocus/absolutefocus.png'),
+
+        "chamuscar": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/scorch/scorch.png'),
+        "caminharSobreAguas": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/waterwalking/waterwalking.png'),
+        "tempestadeCrescente": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/sorcery/gatheringstorm/gatheringstorm.png'),
+    },
+    "Determinacao": {
+        "apertoMortosVivos": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/graspoftheundying/graspoftheundying.png'),
+        "posChoque": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/veteranaftershock/veteranaftershock.png'),
+        "guardiao": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/guardian/guardian.png'),
+
+        "demolir": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/demolish/demolish.png'),
+        "fonteVida": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/fontoflife/fontoflife.png'),
+        "golpeEscudo": require('https://ruined.dev/_next/image/?url=https%3A%2F%2Fruined.dev%2Fstatic%2Fperk-images%2FStyles%2FResolve%2FMirrorShell%2FMirrorShell.png&w=128&q=75'),
+
+        "condicionamento": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/conditioning/conditioning.png'),
+        "ventosRivigorantes": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/secondwind/secondwind.png'),
+        "ossoRevestido": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/boneplating/boneplating.png'),
+
+        "crescimentoExcessivo": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/overgrowth/overgrowth.png'),
+        "revitalizar": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/revitalize/revitalize.png'),
+        "inabalavel": require('https://ruined.dev/_next/image/?url=https%3A%2F%2Fruined.dev%2Fstatic%2Fperk-images%2FStyles%2FSorcery%2FUnflinching%2FUnflinching.png&w=128&q=75'),
+        },
+
+    "Inspiracao": {
+        "aprimoramentoGlacial": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/glacialaugment/glacialaugment.png'),
+        "livroFeiticoDeslacrado": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/unsealedspellbook/unsealedspellbook.png'),
+        "primeiroAtaque": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/firststrike/firststrike.png'),
+
+        "flashtracaoHextec": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/hextechflashtraption/hextechflashtraption.png'),
+        "calcadosMagico": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/magicalfootwear/magicalfootwear.png'),
+        "sincroniaPerfeita": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/perfecttiming/perfecttiming.png'),
+
+        "mercadoFuturo": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/futuresmarket/futuresmarket.png'),
+        "pulverizadorTropas": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/miniondematerializer/miniondematerializer.png'),
+        "entregaBiscoitos": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/biscuitdelivery/biscuitdelivery.png'),
+
+        "perspicaciaCosmica": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/cosmicinsight/cosmicinsight.png'),
+        "velocidadeAproximacao": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/resolve/approachvelocity/approachvelocity.png'),
+        "tonicoDistorcao": require('https://raw.communitydragon.org/latest/game/assets/perks/styles/inspiration/timewarptonic/timewarptonic.png'),
+    },
+
+
+};
+
+export default runeIcons;   
